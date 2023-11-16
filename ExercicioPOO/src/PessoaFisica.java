6783f6b62b1d1647341729ba1420719422330f76
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2023-11-16 10:41:24 -0300

    commit

---
+++ ExercicioPOO/src/PessoaFisica.java
@@ -0,0 +1,20 @@
+public class PessoaFisica extends Pessoa {
+    public String cpf;
+    public String rg;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
---
+++ ExercicioPOO/src/PessoaJuridica.java
@@ -0,0 +1,20 @@
+public class PessoaJuridica extends Pessoa {
+    public String cnpj;
+    public String dataCriacao;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
